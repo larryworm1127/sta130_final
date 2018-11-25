77629eb0b5f3b0730af80ddf04df4d150a91626c
anthonywong611 <athny.wong@mail.utoronto.ca>
2018-11-25 23:31:03 +0000

    added democracy and health data

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -5,7 +5,9 @@ library(maps)
 
 path <- "https://raw.githubusercontent.com/ntaback/UofT_STA130/master/Fall2018/Finalproject/"
 
+democracy_data <- read.csv(paste0(path,"democracyindex2017.csv"))
 internetusers_data <- read_csv(paste0(path,"internetusers_cia2017.csv"))
-gdp_data <- read_csv(paste0(path,"gdpppp_cia2017.csv"))
+gdpppp_data <- read_csv(paste0(path,"gdpppp_cia2017.csv"))
 population_data <- read_csv(paste0(path, "population_cia2017.csv"))
 education_data <- read_csv(paste0(path, "education_cia2017.csv"))
+healthexpend_data <- read_csv(paste0(path,"healthexpend_cia2017.csv"))
