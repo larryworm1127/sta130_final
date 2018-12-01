356756edcb60339d9707ab1fcc80390557cada3e
larryworm1127 <shilerong@gmail.com>
2018-12-01 12:43:15 -0500

    fixed graphs

--- scripts/democracy.R
+++ scripts/democracy.R
@@ -1,7 +1,13 @@
 library(tidyverse)
 library(dplyr)
 
-democracy_data %>% 
+democracy_internet_joint <- democracy_data %>% 
   filter(Rank != "Rank") %>%
-  ggplot() + aes(x = Category, y = Score) +
-  geom_boxplot()
+  inner_join(internetusers_data, by = "Country") %>% 
+  filter(`INTERNET USERS` < 200000000) %>%
+  group_by(Category)
+
+democracy_internet_joint_graph <- ggplot(democracy_internet_joint) + aes(x = Category, y = `INTERNET USERS`) +
+  geom_boxplot()  
+
+
