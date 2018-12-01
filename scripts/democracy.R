be4597f7dd0d54f44793a090e4a2e15e42364f8a
larryworm1127 <shilerong@gmail.com>
2018-12-01 13:07:54 -0500

    minor update

--- scripts/democracy.R
+++ scripts/democracy.R
@@ -9,5 +9,3 @@ democracy_internet_joint <- democracy_data %>%
 
 democracy_internet_joint_graph <- ggplot(democracy_internet_joint) + aes(x = Category, y = `INTERNET USERS`) +
   geom_boxplot()  
-
-
