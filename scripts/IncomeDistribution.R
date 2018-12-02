b15c001be5e46b325ce1923e442b9644b855ec18
anthonywong611 <athny.wong@mail.utoronto.ca>
2018-12-02 23:58:45 +0000

    added democracy and income

--- scripts/IncomeDistribution.R
+++ scripts/IncomeDistribution.R
@@ -5,9 +5,7 @@ library(partykit)
 
 income_internet_join <- incomedistribution_data %>%
   inner_join(population_internet_join, by = "Country") 
-
-income_fit <- rpart(Density ~ `DISTRIBUTION OF FAMILY INCOME - GINI INDEX`, 
-                    income_internet_join, control = rpart.control(cp = 0.02))
-
-income_tree <- plot(as.party(income_fit), type = "simple", gp = gpar(cex = 0.8))
   
+income_regression <- income_internet_join %>%
+  ggplot() + aes(x = `DISTRIBUTION OF FAMILY INCOME - GINI INDEX`, y = Percentage) +
+  geom_point()
--- scripts/democracy.R
+++ scripts/democracy.R
@@ -1,11 +1,22 @@
 library(tidyverse)
 library(dplyr)
+library(rpart)
+library(partykit)
 
 democracy_internet_joint <- democracy_data %>% 
   filter(Rank != "Rank") %>%
   inner_join(internetusers_data, by = "Country") %>% 
-  filter(`INTERNET USERS` < 200000000) %>%
-  group_by(Category)
-
-democracy_internet_joint_graph <- ggplot(democracy_internet_joint) + aes(x = Category, y = `INTERNET USERS`) +
+  inner_join(population_data, by = "Country") %>% 
+  mutate(PoliticalParticipation = as.numeric_version(Politicalparticipation)) %>%
+  mutate(Percentage = `INTERNET USERS` / POPULATION) %>%
+  mutate(Density = ifelse(Percentage < 1/3 , "low", 
+                   ifelse(Percentage >= 1/3 & Percentage < 2/3, "medium", "high"))) %>%
+  mutate(PoliticalParticipationLevel = ifelse(PoliticalParticipation < 10/3, "passive", ifelse(PoliticalParticipation >= 10/3 & PoliticalParticipation < 20/3, "neutral", "active"))) %>%
+  filter(`INTERNET USERS` < 200000000)
+ 
+democracy_fit <- rpart(Density ~ Category + PoliticalParticipationLevel, democracy_internet_joint)
+democracy_tree <- plot(as.party(democracy_fit), type = "simple", gp = gpar(cex = 0.8))
+   
+democracy_internet_joint_graph <- ggplot(democracy_internet_joint) + 
+  aes(x = Category, y = `INTERNET USERS`) +
   geom_boxplot()
