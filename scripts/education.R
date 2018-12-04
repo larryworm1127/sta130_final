4b57cb29c7308e2c44b8b7745718056bc64f6186
larryworm1127 <shilerong@gmail.com>
2018-12-04 18:25:14 -0500

    added education

---
+++ scripts/education.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+library(dplyr)
+
+
+# Wrangle data
+education_internet_join <- internetusers_data %>%
+  inner_join(education_data, by = "Country") %>%
+  select(Country, `INTERNET USERS`, `(% OF GDP)`)
+
+education_internet_density_join <- population_internet_join %>%
+  inner_join(education_data, by = "Country") %>%
+  select(Country, POPULATION, `INTERNET USERS`, Percentage, Density, `(% OF GDP)`)
+
+
+# Plot graphs
+education_internet_graph <- ggplot(education_internet_join, aes(x = `(% OF GDP)`, y = `INTERNET USERS`)) +
+  geom_point() +
+  labs(title = "% of GDP on Education vs. Total Number of Internet Users", x = "% of GDP on Education", y = "Internet Users") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+education_internet_density_graph <- ggplot(education_internet_density_join, aes(x = `(% OF GDP)`, y = `Percentage`)) +
+  geom_point() +  geom_smooth(method = "lm") +
+  labs(title = "% of GDP on Education vs. % Internet Users of the Total Population", x = "% of GDP on Education", y = "% Population") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+
--- scripts/health.R
+++ scripts/health.R
@@ -11,22 +11,18 @@ lifeexpect_internet_density_join <- population_internet_join %>%
   select(Country, POPULATION, `INTERNET USERS`, Percentage, Density, `(YEARS)`) %>%
   filter(Percentage < 1.0)
 
+
 # Plot graphs
 lifeexpect_internet_graph <- ggplot(lifeexpect_internet_join, aes(x = `(YEARS)`, y = `INTERNET USERS`)) +
   geom_point() +
   labs(title = "Life Expectancy vs. Total Number of Internet Users", x = "Life Expectancy (Years)", y = "Internet Users") +
   theme(plot.title = element_text(hjust = 0.5))
 
-lifeexpect_internet_density_graph <- ggplot(lifeexpect_internet_density_join, aes(x = `(YEARS)`, y = `Percentage`)) +
-  geom_point() +
-  labs(title = "Life Expectancy vs. % Internet Users of the Total Population", x = "Life Expectancy (Years)", y = "% Population") +
-  theme(plot.title = element_text(hjust = 0.5))
-
-
-# Plot regression model
 lifeexpect_internet_density_graph_line <- ggplot(lifeexpect_internet_density_join, aes(x = `(YEARS)`, y = `Percentage`)) +
   geom_point() + geom_smooth(method = "lm") +
   labs(title = "Life Expectancy vs. % Internet Users of the Total Population", x = "Life Expectancy (Years)", y = "% Population") +
   theme(plot.title = element_text(hjust = 0.5))
 
+
+# Plot regression model
 lifeexpect_internet_model <- lm(Percentage ~ `(YEARS)`, data = lifeexpect_internet_density_join)
