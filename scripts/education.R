1b0ed88ed1bcc01221a2cc5657a58d06acd182a2
larryworm1127 <shilerong@gmail.com>
2018-12-05 15:47:24 -0500

    finished education

--- scripts/education.R
+++ scripts/education.R
@@ -19,46 +19,4 @@ education_internet_density_graph <- ggplot(education_internet_join, aes(x = `(% 
   labs(title = "% of GDP on Education vs. % Internet Users", x = "% of GDP on Education", y = "% Population") +
   theme(plot.title = element_text(hjust = 0.5))
 
-mike
-
-library(tidyverse)
-library(readr)
-library(maps)
-library(dplyr)
-library(ggplot2)
-
-get_num_from_char <- function(data_vector) {
-  result <- c()
-  for (item in data_vector) {
-    item_split <- strsplit(item, "")[[1]]
-    sanitized_item <- paste(item_split [! item_split %in% unwanted_char], collapse = '')
-    result <- c(result, as.categorical(sanitized_item))
-  }
-  
-  path <- "https://raw.githubusercontent.com/ntaback/UofT_STA130/master/Fall2018/Finalproject/"
-  
-  
-  internetusers_data <- read_csv(paste0(path,"internetusers_cia2017.csv"))
-  education_data <- read_csv(paste0(path, "education_cia2017.csv"))
-  
-  internetusers_edu_joint<-inner_join(x=internetusers_data , y = education_data , by= "Country"  ) %>%
-    mutate (EDU = internetusers_edu_joint$`(% OF GDP)`) %>%
-    mutate (edu_cat = ifelse(EDU > 10.5, "5",
-                             ifelse(EDU <=10.5 & EDU > 8, "4",
-                                    ifelse(EDU <= 8 & EDU > 5.5, "3",
-                                           ifelse(EDU <= 5.5 & EDU > 3, "2","1" )))))
-  
-  internetusers_edu_joint_graph <- ggplot(internetusers_edu_joint) + 
-    aes(x = edu_cat, y = internetusers_edu_joint$`INTERNET USERS`) +geom_boxplot()
-  internetusers_edu_joint_graph
-  
-  internetusers_edu_joint
-  
-  //fit <-lm(INTERNET USERS ~ %OFGDP, data = education_data)
-  y_test <- education_data_test$Country
-  summary(lm(`INTERNET USERS`  ~ `(% OF GDP)` , data = internetusers_edu_joint))$coefficients
-  summary(lm(`INTERNET USERS`  ~ `(% OF GDP)` , data = internetusers_edu_joint))$r.squared
-  
-
-
-
+education_internet_fit <- summary(lm(`Percentage` ~ `(% OF GDP)`, data = education_internet_join))
