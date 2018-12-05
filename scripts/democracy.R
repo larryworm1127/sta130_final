2659c1362a521bb2b6288e02295aa61dba4dfb71
anthonywong611 <athny.wong@mail.utoronto.ca>
2018-12-05 23:17:14 +0000

    added confusion matrix

--- scripts/democracy.R
+++ scripts/democracy.R
@@ -12,19 +12,31 @@ democracy_internet_joint <- democracy_data %>%
   mutate(PoliticalParticipationLevel = ifelse(PoliticalParticipation < 10/3, "passive", ifelse(PoliticalParticipation >= 10/3 & PoliticalParticipation < 20/3, "neutral", "active"))) %>%
   filter(`INTERNET USERS` < 200000000)
 
+dc_joint <- democracy_internet_joint
 
 # Create classficiation tree
 democracy_fit <- rpart(Density ~ Category + PoliticalParticipationLevel, democracy_internet_joint)
 democracy_tree <- plot(as.party(democracy_fit), type = "simple", gp = gpar(cex = 0.8))
 
+# Testing and Training Set
+set.seed(364)
+n <- nrow(democracy_internet_joint)
 
+## random sample of 40% of row indexes
+test_idx <- sample.int(n, size = round(0.4 * n))
+## training data 
+train <- dc_joint[-test_idx, ]
+## testing data
+test <- dc_joint[test_idx, ]
 
+## predicting tree
+tree <-rpart(Density ~ Category + PoliticalParticipationLevel, data = train,
+             parms = list(split = "gini"))
 
+## confusion matrix
+predicted_tree <- predict(object = tree, newdata = test, type = "class")
+confusion_matrix <- table(predicted_tree, test$Density)
 
-# Plot graphs   
-democracy_internet_joint_graph <- ggplot(democracy_internet_joint) + 
-  aes(x = Category, y = `INTERNET USERS`) +
-  geom_boxplot() +
-  theme(plot.title = element_text(hjust = 0.5))
+accuracy <- 31/61
 
    
